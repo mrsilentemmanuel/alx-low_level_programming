541fe90c0bdd7b482dfef66b2e0c4a91796997c5
Silent Emmanuel <44226645+mrsilentemmanuel@users.noreply.github.com>
2023-03-16 22:05:52 +0200

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# inlcude <stdio.h>
+/**
+ * main - Entry point
+ * Return - Always 0 (success)
+ * /
+ int main(void)
+ {
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
+ }
+
